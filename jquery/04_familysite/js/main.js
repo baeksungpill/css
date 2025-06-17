48f6eacc0dcd4637dae8bc85e5b9432d7e70e936
baeksungpill <sp7998@naver.com>
2025-06-17 17:22:53 +0900

    Add files via upload

---
+++ jquery/04_familysite/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+$(".group .family1>button").click(function(){
+    $(".group .family1>ul").toggle();
+})
+$(".group .family2>button").click(function(){
+    $(".group .family2>ul").toggle();
+})
+
+
+
+
+})
