ed6b20b8113b58cc3875e811da0e05a673b46a99
baeksungpill <sp7998@naver.com>
2025-06-23 16:26:29 +0900

    Add files via upload

---
+++ jquery/10_bannerslide/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+ $('.list').slick({
+    // 자동실행옵션
+    autoplay: true,
+    // 한화면에 보일 갯수
+    slidesToShow:2,
+    // next버튼 2개씩 가게 하는거
+    slidesToScroll:1,
+    // 좌우버튼 숨기기
+    arrows:false,
+    // 페이지 버튼 보이기
+    dots:true
+});
+
+
+
+})
