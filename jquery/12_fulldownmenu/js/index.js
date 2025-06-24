581bd6bebf1509d84e2db20394303797ab5f3c64
baeksungpill <sp7998@naver.com>
2025-06-24 16:05:05 +0900

    Add files via upload

--- jquery/12_fulldownmenu/js/index.js
+++ jquery/12_fulldownmenu/js/index.js
@@ -3,12 +3,16 @@ $(function(){
         $("#header_wrap").stop().animate({"height":380},300);
         // 2단메뉴를 보이게함
         $(".depth2").show();
+        $("#header_wrap").css({'border-bottom':'1px solid #c3c3c3'});
 
     })
-    // 전체 박스를 벗어나면
+    // 전체 박스를 벗어나면 다시 110px으로 바뀜
     $("#header_wrap").mouseleave(function(){
         $("#header_wrap").stop().animate({"height":110},300);
+        // 2단메뉴 숨김
         $(".depth2").hide();
+        // $("#header_wrap").css({'border-bottom':'none'});
+        
 
     })
 
