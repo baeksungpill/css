6a3eeb2351acc7609b5245072cc4becd5cddabdc
baeksungpill <sp7998@naver.com>
2025-07-01 16:47:29 +0900

    Add files via upload

---
+++ jquery/14_tap/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $(".list2").hide();
+    $("ul.menu li").click(function(){
+        $(".menu li").removeClass("on");
+        $(this).addClass("on");
+        let num = $(this).index();
+        console.log(num);
+        $(".list").hide();
+        $(".list").eq(num).show();
+    })
+})
