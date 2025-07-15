978d09ac8aa5c1568a101a0b39ababc94516cfa5
baeksungpill <sp7998@naver.com>
2025-07-15 11:13:14 +0900

    Add files via upload

--- jquery/08_mobileslide(subway)/js/main.js
+++ jquery/08_mobileslide(subway)/js/main.js
@@ -70,4 +70,69 @@ $("ul.right_icon li").click(function(){
   $("ul.left_icon li").removeClass('on')
   $(this).addClass("on")
 })
+
+
+// 쿠키정보를 확인해서 쿠키정보가 있으면 숨기고, 없으면 열림
+if(GetCookie('subway')=='today'){
+  $('.popup').hide();
+}else{
+  $('.popup').show();
+}
+
+// 팝업창 스크립트
+$("#today_close,.popup .close label").click(function(){
+  // setCookie 함수를 호출
+  if($("#today_close").is(':checked')){
+  // Cookie정보를 생성한후 popup창은 안보이게 숨김
+  setCookie('subway','today',1);
+  $(".popup").hide()
+  }else{
+  $('.popup').hide()
+  }
+})
+
+// 닫기 버튼 클릭시 팝업창 숨김
+$('.popup .close .txt_btn').click(function(){
+  $('.popup').hide();
+
+})
+
+
+// GetCookie (Cookie 정보)
+function GetCookie(name){
+				var value=null, search=name+"=";
+				if(document.cookie.length > 0){
+					var offset=document.cookie.indexOf(search);
+					if(offset != -1){
+						offset+=search.length;
+						var end=document.cookie.indexOf(";", offset);
+						if(end == -1) end=document.cookie.length;
+						value=unescape(document.cookie.substring(offset, end));
+					}
+				} return value;
+			}
+			// SetCookie
+			function setCookie(name, value, expiredays){
+				var days=10;
+				if(days){
+					var date=new Date(); 
+					date.setTime(date.getTime()+(days*24*60*60*1000));
+					var expires="; expires="+date.toGMTString();
+				}else{
+					var expires=""; 
+				}
+				document.cookie=name+"="+value+expires+"; path=/";
+			}
+
+
+
+
+
+
+
+
+
+
+
+
 })
