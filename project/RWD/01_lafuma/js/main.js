836ca3b06bdffaf2bca9725d7302c9a1dc2bf295
baeksungpill <sp7998@naver.com>
2025-07-16 11:45:44 +0900

    Add files via upload

--- project/RWD/01_lafuma/js/main.js
+++ project/RWD/01_lafuma/js/main.js
@@ -31,8 +31,36 @@ $(function () {
       el: ".swiper-scrollbar",
     },
   });
-
-
-
-
 });
+
+
+// css가 아닌 js에서 content 사진 hover걸때
+$(function(){
+$(".content ul.list li").mouseenter(function(){
+  let num = $(this).find("img").attr('src');
+  console.log(num);
+  // replace함수(바꾸는 함수) _ot로 되어있는 파일을 _ov로 바꿔라
+  let num2 = num.replace('_ot','_ov');
+  console.log(num2);
+
+  $(this).find('img').attr('src',num2);
+})
+
+$(".content ul.list li").mouseleave(function(){
+  let num = $(this).find("img").attr('src');
+  console.log(num);
+  // replace함수(바꾸는 함수) _ot로 되어있는 파일을 _ov로 바꿔라
+  let num2 = num.replace('_ov','_ot');
+  console.log(num2);
+
+  $(this).find('img').attr('src',num2);
+})
+})
+
+$(function(){
+  $('.btn_family').click(function(){
+    $(".family_box .list").slideToggle();
+    $(this).toggleClass("on");
+  })
+
+})
